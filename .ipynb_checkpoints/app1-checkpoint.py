9c5a2f6da7090b8abbf8e952241ce6c654babdbb
benjamin-connors <91761308+benjamin-connors@users.noreply.github.com>
2024-08-13 10:32:09 -0700

    cleaning up repo

--- .ipynb_checkpoints/app1-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.write('Welcome to app1!')
-st.write('app1 secret is...' + st.secrets['app1']['the_secret'])
--- .ipynb_checkpoints/app2-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.write('Welcome to app2!')
-st.write('app2 secret is...' + st.secrets['app2']['the_secret'])
